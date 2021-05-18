d227ad12cbcd3cedcef47709e95403540a4598b4
Breanna Moore <moorbrea@oregonstate.edu>
2021-05-17 19:38:03 -0700

    cors added

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
 const mysql = require('./dbcon');
 
+app.use(cors());
 app.use(express.static('public'));
 app.set('port', 6531);
 app.use(express.json());
